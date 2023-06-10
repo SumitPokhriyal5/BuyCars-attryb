564616c9d638c110b827766d3728729c5afac246
Sumit Pokhriyal <sumitstan1998@gmail.com>
2023-06-10 21:23:00 +0530

    Exclude dist directory

--- backend/dist/config/db.js
+++
@@ -1,6 +0,0 @@
-import mongoose from 'mongoose';
-import * as dotenv from 'dotenv';
-dotenv.config();
-const connectDB = mongoose.connect(process.env.DATABASE_URL);
-export default connectDB;
-//# sourceMappingURL=db.js.map
